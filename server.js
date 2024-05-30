93c50965fc189d5f6ae619bb711d23f3977eec19
KidKaffeine <vanessa.m.a1989@gmail.com>
2024-05-30 09:23:46 +0200

    added nodemon and express as dependencies, created express app listening on port 8000, created gitignore file in root

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("ai")
+const express = require("express")
+
+const app = express()
+const port = 8000
+
+app.get("/" , (req, res) => {
+res.send("hai")
+})
+
+app.listen(port, () => {
+    console.log("hai")
+})
