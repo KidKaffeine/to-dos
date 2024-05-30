627fb012f3d97f532eec9d1def1522e1ae7ca291
KidKaffeine <vanessa.m.a1989@gmail.com>
2024-05-30 20:34:50 +0200

    added doten to dependencies, created env file with mongo uri, created db folder with db connect file

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express")
+require('dotenv').config()
+const dbConnect = require("./db/dbConnect/dbConnect")
 
 const app = express()
-const port = 8000
+const port = 7000
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
+
+app.listen(port, async () => {
+    console.log(`Listening on port ${port}!`)
+    dbConnect()
 })
+
