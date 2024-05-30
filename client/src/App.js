7f4d2ffc75258fc0af07941b9f738fa9adfd0b7d
KidKaffeine <vanessa.m.a1989@gmail.com>
2024-05-30 06:54:44 +0200

    created ui container component and header component, added background color to app, rendered header in app

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-<h1>Hai.</h1>
+      <Header title={"shit to do"}/>
     </div>
   );
 }
