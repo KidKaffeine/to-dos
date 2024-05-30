8f7936bc1f020b7ab61bcfb70174cff04b39a1c8
KidKaffeine <vanessa.m.a1989@gmail.com>
2024-05-30 07:49:27 +0200

    added link to add task icon, created error page, created pages css folder, added css to error page and root, added error page as error element to root

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import {
   faStar,
   faMagnifyingGlass,
 } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
@@ -12,24 +13,20 @@ export default function Navbar() {
       <nav className={styles.navBar}>
         <ul className={styles.navList}>
           <li key="starIcon">
-            <FontAwesomeIcon
-              icon={faStar}
-              size="2x"
-            />
+            <FontAwesomeIcon icon={faStar} size="2x" />
           </li>
           <li key="addIcon">
+            <Link to="/addTask">
               <FontAwesomeIcon
                 icon={faPlusCircle}
                 size="2x"
                 beatFade
                 color="yellow"
               />
+            </Link>
           </li>
           <li key="searchIcon">
-            <FontAwesomeIcon
-              icon={faMagnifyingGlass}
-              size="2x"
-            />
+            <FontAwesomeIcon icon={faMagnifyingGlass} size="2x" />
           </li>
         </ul>
       </nav>
