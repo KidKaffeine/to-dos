008b20ee1a36584e356ec144812284aa08a2b18e
KidKaffeine <vanessa.m.a1989@gmail.com>
2024-05-30 11:36:55 +0200

    removed express and nodemon as dependencies, deleted node modules from root, added env files to it, removed express app from server.js

--- server.js
+++ server.js
@@ -1,12 +1,1 @@
-const express = require("express")
-
-const app = express()
-const port = 8000
-
-app.get("/" , (req, res) => {
-res.send("hai")
-})
-
-app.listen(port, () => {
-    console.log("hai")
-})
+console.log("hai")
